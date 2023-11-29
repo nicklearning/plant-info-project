0590d59a0a47b8ffae7022b353fadd8a17b31a84
JB <bradburyjesse7@gmail.com>
2023-11-28 21:29:00 -0700

    added function that pulls data from API and displays it on page.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -19,6 +19,53 @@ var handleSearch = function () {
         })
 };
 
+var plantId = "352"
+var getPlantInfo = function () {
+    var requestDetailsURL = "https://perenual.com/api/species/details/" + plantId + "?key=sk-VfPS655d61aa10f743067";
+
+    fetch(requestDetailsURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+
+            // Added function that will capitolize the first letter of the Name
+            var plantNameEl = document.querySelector("#name-search");
+            function capitalizeFirstLetter(string) {
+                return string.charAt(0).toUpperCase() + string.slice(1);
+            }
+            var capitalizedPlantName = capitalizeFirstLetter(data.common_name);
+            plantNameEl.textContent = capitalizedPlantName;
+
+            var scientificNameEl = document.querySelector("#latin-search");
+            scientificNameEl.textContent = data.scientific_name;
+
+            var plantTypeEl = document.querySelector("#type-search");
+            plantTypeEl.textContent = data.type + ", " + data.cycle;
+
+            var plantFamilyEl = document.querySelector("#family-search");
+            plantFamilyEl.textContent = data.family;
+
+            var plantDescriptionEl = document.querySelector("#description-search");
+            plantDescriptionEl.textContent = data.description;
+
+            var wateringDetailsEl = document.querySelector("#watering-details");
+            wateringDetailsEl.textContent = data.watering;
+
+            var sunDetailsEl = document.querySelector("#sun-details");
+            sunDetailsEl.textContent = data.sunlight;
+
+            var sunDetailsEl = document.querySelector("#maturity-details");
+            sunDetailsEl.textContent = data.dimension;
+
+
+
+
+            console.log(data);
+
+        })
+};
+getPlantInfo();
 
 searchForm.addEventListener("submit", function (event) {
     event.preventDefault();
