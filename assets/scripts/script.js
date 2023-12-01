7a3b7726f4839e228753b73151bcfd608b89a9d1
JB <bradburyjesse7@gmail.com>
2023-11-30 21:13:14 -0700

    adding Picture from API and future functions for populating plant details from selected items.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -62,24 +62,31 @@ var getPlantInfo = function (plantId) {
             return response.json();
         })
         .then(function (data) {
-            // TODO: get the HREF for the plant & set it to the image. Get the Name of the plant, Scientific name.  
+            // TODO: get the PLant ID from the selected plant from the carousel. 
             console.log(data)
-            
+
         })
-        getPlantDescription();
+    displayPlantDetails();
 }
 
 
 
-var plantId = "352"
-var getPlantDescription = function () {
-    var requestDetailsURL = "https://perenual.com/api/species/details/" + plantId + "?sk-irI665693a01c0c353234";
+var plantId = "1026"
+var displayPlantDetails = function () {
+    var requestDetailsURL = "https://perenual.com/api/species/details/" + plantId + "?key=sk-irI665693a01c0c353234";
 
     fetch(requestDetailsURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
+
+            var plantPic = document.querySelector("#plant-pic")
+            var plantImg = document.createElement("img");
+            plantImg.src = data.default_image.small_url;
+            plantImg.alt = "Photo of a " + capitalizedPlantName;
+
+            plantPic.appendChild(plantImg)
 
             // Added function that will capitolize the first letter of the Name
             var plantNameEl = document.querySelector("#name-search");
@@ -117,7 +124,7 @@ var getPlantDescription = function () {
 
         })
 };
-// getPlantDescription();
+displayPlantDetails();
 
 searchForm.addEventListener("submit", function (event) {
     event.preventDefault();
